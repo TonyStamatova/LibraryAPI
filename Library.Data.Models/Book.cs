e5f3757d72e52c0aaa9761d47c0f5f45ec9047fb
TonyStamatova <a.stamatova@abv.bg>
2020-02-08 18:01:33 +0200

    Added properties of Book entity

--- Library.Data.Models/Book.cs
+++ Library.Data.Models/Book.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
+using Library.Data.Models.Enumerations;
 
 namespace Library.Data.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public GenreType GenreType { get; set; }
+
+        public Language Language { get; set; }
+
+        public ICollection<string> KeyWords { get; set; } = new HashSet<string>();
+
+        public string Description { get; set; }
     }
 }
