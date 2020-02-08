6d99e2b25ccd377ed325c8d7f708f367312a8b35
TonyStamatova <a.stamatova@abv.bg>
2020-02-08 15:30:41 +0200

    Added enums for Genre

---
+++ Library.Data.Models/Enumerations/GenreType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Library.Data.Models.Enumerations
+{
+    public enum GenreType
+    {
+        [Description("Other")]
+        Other = 0,
+
+        [Description ("Fiction")]
+        Fiction = 1,
+
+        [Description ("Non-fiction")]
+        NonFiction = 2
+    }
+}
