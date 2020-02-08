17e386c36ce7519790dbc79036cb664f8b8e7838
TonyStamatova <a.stamatova@abv.bg>
2020-02-08 20:53:47 +0200

    Defined DbSets in Context

--- Library.Data/LibraryContext.cs
+++ Library.Data/LibraryContext.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Data.Entity;
+﻿using System.Data.Entity;
+
+using Library.Data.Models;
 
 namespace Library.Data
 {
     public class LibraryContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }        
     }
 }
