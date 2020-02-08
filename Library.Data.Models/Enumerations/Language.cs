ce650760df88ed0e049ce2d08c4e0f014e224e09
TonyStamatova <a.stamatova@abv.bg>
2020-02-08 16:37:30 +0200

    Added enum for Language

---
+++ Library.Data.Models/Enumerations/Language.cs
@@ -0,0 +1,322 @@
+﻿using System.ComponentModel;
+
+namespace Library.Data.Models.Enumerations
+{
+    public enum Language
+    {
+        [Description("Another")]
+        Another = 0,
+
+        [Description("Afrikaans")]
+        Afrikaans = 1,
+
+        [Description("Albanian")]
+        Albanian = 2,
+
+        [Description("Amharic")]
+        Amharic = 3,
+
+        [Description("Arabic")]
+        Arabic = 4,
+
+        [Description("Armenian")]
+        Armenian = 5,
+
+        [Description("Azerbaijani")]
+        Azerbaijani = 6,
+
+        [Description("Basque")]
+        Basque = 7,
+
+        [Description("Belarusian")]
+        Belarusian = 8,
+
+        [Description("Bengali")]
+        Bengali = 9,
+
+        [Description("Bosnian")]
+        Bosnian = 10,
+
+        [Description("Bulgarian")]
+        Bulgarian = 11,
+
+        [Description("Burmese")]
+        Burmese = 12,
+
+        [Description("Catalan")]
+        Catalan = 13,
+
+        [Description("Cebuano")]
+        Cebuano = 14,
+
+        [Description("Chichewa")]
+        Chichewa = 15,
+
+        [Description("Chinese (Simplified)")]
+        ChineseSimplified = 16,
+
+        [Description("Chinese (Traditional)")]
+        ChineseTraditional = 17,
+
+        [Description("Corsican")]
+        Corsican = 18,
+
+        [Description("Croatian")]
+        Croatian = 19,
+
+        [Description("Czech")]
+        Czech = 20,
+
+        [Description("Danish")]
+        Danish = 21,
+
+        [Description("Dutch")]
+        Dutch = 22,
+
+        [Description("English")]
+        English = 23,
+
+        [Description("Esperanto")]
+        Esperanto = 24,
+
+        [Description("Estonian")]
+        Estonian = 25,
+
+        [Description("Filipino")]
+        Filipino = 26,
+
+        [Description("Finnish")]
+        Finnish = 27,
+
+        [Description("French")]
+        French = 28,
+
+        [Description("Galician")]
+        Galician = 29,
+
+        [Description("Georgian")]
+        Georgian = 30,
+
+        [Description("German")]
+        German = 31,
+
+        [Description("Greek")]
+        Greek = 32,
+
+        [Description("Gujarati")]
+        Gujarati = 33,
+
+        [Description("Haitian Creole")]
+        HaitianCreole = 34,
+
+        [Description("Hausa")]
+        Hausa = 35,
+
+        [Description("Hawaiian")]
+        Hawaiian = 36,
+
+        [Description("Hebrew")]
+        Hebrew = 37,
+
+        [Description("Hindi")]
+        Hindi = 38,
+
+        [Description("Hmong")]
+        Hmong = 39,
+
+        [Description("Hungarian")]
+        Hungarian = 40,
+
+        [Description("Icelandic")]
+        Icelandic = 41,
+
+        [Description("Igbo")]
+        Igbo = 42,
+
+        [Description("Indonesian")]
+        Indonesian = 43,
+
+        [Description("Irish")]
+        Irish = 44,
+
+        [Description("Italian")]
+        Italian = 45,
+
+        [Description("Japanese")]
+        Japanese = 46,
+
+        [Description("Javanese")]
+        Javanese = 47,
+
+        [Description("Kannada")]
+        Kannada = 48,
+
+        [Description("Kazakh")]
+        Kazakh = 49,
+
+        [Description("Khmer")]
+        Khmer = 50,
+
+        [Description("Korean")]
+        Korean = 51,
+
+        [Description("Kurdish (Kurmanji)")]
+        KurdishKurmanji = 52,
+
+        [Description("Kyrgyz")]
+        Kyrgyz = 53,
+
+        [Description("Lao")]
+        Lao = 54,
+
+        [Description("Latin")]
+        Latin = 55,
+
+        [Description("Latvian")]
+        Latvian = 56,
+
+        [Description("Lithuanian")]
+        Lithuanian = 57,
+
+        [Description("Luxembourgish")]
+        Luxembourgish = 58,
+
+        [Description("Macedonian")]
+        Macedonian = 59,
+
+        [Description("Malagasy")]
+        Malagasy = 60,
+
+        [Description("Malay")]
+        Malay = 61,
+
+        [Description("Malayalam")]
+        Malayalam = 62,
+
+        [Description("Maltese")]
+        Maltese = 63,
+
+        [Description("Maori")]
+        Maori = 64,
+
+        [Description("Marathi")]
+        Marathi = 65,
+
+        [Description("Mongolian")]
+        Mongolian = 66,
+
+        [Description("Nepali")]
+        Nepali = 67,
+
+        [Description("Norwegian")]
+        Norwegian = 68,
+
+        [Description("Pashto")]
+        Pashto = 69,
+
+        [Description("Persian")]
+        Persian = 70,
+
+        [Description("Polish")]
+        Polish = 71,
+
+        [Description("Portuguese")]
+        Portuguese = 72,
+
+        [Description("Punjabi")]
+        Punjabi = 73,
+
+        [Description("Romanian")]
+        Romanian = 74,
+
+        [Description("Russian")]
+        Russian = 75,
+
+        [Description("Samoan")]
+        Samoan = 76,
+
+        [Description("Scots Gaelic")]
+        ScotsGaelic = 77,
+
+        [Description("Serbian")]
+        Serbian = 78,
+
+        [Description("Sesotho")]
+        Sesotho = 79,
+
+        [Description("Shona")]
+        Shona = 80,
+
+        [Description("Sindhi")]
+        Sindhi = 81,
+
+        [Description("Sinhala")]
+        Sinhala = 82,
+
+        [Description("Slovak")]
+        Slovak = 83,
+
+        [Description("Slovenian")]
+        Slovenian = 84,
+
+        [Description("Somali")]
+        Somali = 85,
+
+        [Description("Spanish")]
+        Spanish = 86,
+
+        [Description("Sundanese")]
+        Sundanese = 87,
+
+        [Description("Swahili")]
+        Swahili = 88,
+
+        [Description("Swedish")]
+        Swedish = 89,
+
+        [Description("Tajik")]
+        Tajik = 90,
+
+        [Description("Tamil")]
+        Tamil = 91,
+
+        [Description("Telugu")]
+        Telugu = 92,
+
+        [Description("Thai")]
+        Thai = 93,
+
+        [Description("Turkish")]
+        Turkish = 94,
+
+        [Description("Ukrainian")]
+        Ukrainian = 95,
+
+        [Description("Urdu")]
+        Urdu = 96,
+
+        [Description("Uzbek")]
+        Uzbek = 97,
+
+        [Description("Vietnamese")]
+        Vietnamese = 98,
+
+        [Description("Welsh")]
+        Welsh = 99,
+
+        [Description("West Frisian")]
+        WestFrisian = 100,
+
+        [Description("Xhosa")]
+        Xhosa = 101,
+
+        [Description("Yiddish")]
+        Yiddish = 102,
+
+        [Description("Yoruba")]
+        Yoruba = 103,
+
+        [Description("Zulu")]
+        Zulu = 104
+    }
+}
