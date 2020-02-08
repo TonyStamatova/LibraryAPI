0f3e7e561106e057f9443cb800b4dfd26ddf9f3e
TonyStamatova <a.stamatova@abv.bg>
2020-02-08 16:08:50 +0200

    Added model class for Author

---
+++ Library.Data.Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Data.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Pseudonym { get; set; }
+
+        public string About { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new HashSet<Book>();
+    }
+}
