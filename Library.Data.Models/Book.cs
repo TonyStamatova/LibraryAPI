b47f8ca94d81ff1fdc1493c2dfd0a0587b0db4ea
TonyStamatova <a.stamatova@abv.bg>
2020-02-09 12:39:36 +0200

    Made the Genre property a collection of several genres

--- Library.Data.Models/Book.cs
+++ Library.Data.Models/Book.cs
@@ -12,7 +12,7 @@ namespace Library.Data.Models
         public int AuthorId { get; set; }
         public Author Author { get; set; }
 
-        public Genre Genre { get; set; }
+        public ICollection<Genre> Genre { get; set; } = new HashSet<Genre>();
 
         public GenreType GenreType { get; set; }
 
