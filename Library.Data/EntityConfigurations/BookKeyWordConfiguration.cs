96cafd0a8611eba3a90dc4c4abbf7128f45e5a79
TonyStamatova <a.stamatova@abv.bg>
2020-02-15 13:57:26 +0200

    Added config for mapping table

---
+++ Library.Data/EntityConfigurations/BookKeyWordConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+using Library.Data.Models;
+
+namespace Library.Data.EntityConfigurations
+{
+    internal class BookKeyWordConfiguration : EntityTypeConfiguration<BookKeyWord>
+    {
+        internal BookKeyWordConfiguration()
+        {
+            this.HasKey(bkw => new { bkw.BookId, bkw.KeyWordId });
+        }
+    }
+}
