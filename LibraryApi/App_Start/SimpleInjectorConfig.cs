299833dd7ecafec8d66b2e3be72b1569714eddfe
TonyStamatova <a.stamatova@abv.bg>
2020-02-23 19:19:34 +0200

    Fixed controller registration

--- LibraryApi/App_Start/SimpleInjectorConfig.cs
+++ LibraryApi/App_Start/SimpleInjectorConfig.cs
@@ -1,17 +1,19 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Mvc;
 
 using Library.Data;
 using Library.Data.Repositories;
 using Library.Data.Repositories.Contracts;
-
+using LibraryApi.Controllers;
 using SimpleInjector;
 using SimpleInjector.Integration.Web;
 using SimpleInjector.Integration.Web.Mvc;
+using SimpleInjector.Integration.WebApi;
 
 namespace LibraryApi
-{ 
+{
     public class SimpleInjectorConfig
     {
         public static void Register()
@@ -21,11 +23,13 @@ namespace LibraryApi
 
             //register services here
             container.Register(() => new LibraryContext(), Lifestyle.Scoped);
-            container.Register<IBookRepository, BookRepository>();
+            container.Register<IBookRepository, BookRepository>(Lifestyle.Scoped);
 
-            container.RegisterWebApiControllers(new HttpConfiguration(), Assembly.GetExecutingAssembly());
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
 
-            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
         }
     }
 }
---
+++ LibraryApi/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using Library.Data.Repositories.Contracts;
+
+namespace LibraryApi.Controllers
+{
+    public class BookController : ApiController
+    {
+        private readonly IBookRepository repo;
+
+        public BookController(IBookRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await repo.GetAllBooksByGenreAsync();
+                return Ok(result);
+            }
+            catch (ArgumentException)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
