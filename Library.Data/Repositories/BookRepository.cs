72de09dfe54a00e36ec39d8cf20b8e85e00707ea
TonyStamatova <a.stamatova@abv.bg>
2020-02-23 19:18:26 +0200

    Added a method to book repo

--- Library.Data/Repositories/BookRepository.cs
+++ Library.Data/Repositories/BookRepository.cs
@@ -1,8 +1,34 @@
-﻿using Library.Data.Repositories.Contracts;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Library.Data.Models;
+using Library.Data.Models.Enumerations;
+using Library.Data.Repositories.Contracts;
 
 namespace Library.Data.Repositories
 {
     public class BookRepository : IBookRepository
     {
+        private readonly LibraryContext context;
+
+        public BookRepository(LibraryContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Book[]> GetAllBooksByGenreAsync(string genre = "fiction")
+        {
+            if (Enum.TryParse(genre, true, out Genre genreEnum))
+            {
+                throw new ArgumentException("There are no records for books of this genre.");
+            }
+
+            IQueryable<Book> query = context.Books
+                .Where(b => b.Genre.Contains(genreEnum));
+
+            return await query.ToArrayAsync();
+        }
     }
 }
--- Library.Data/Repositories/Contracts/IBookRepository.cs
+++ Library.Data/Repositories/Contracts/IBookRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Library.Data.Repositories.Contracts
+﻿using System.Threading.Tasks;
+
+using Library.Data.Models;
+
+namespace Library.Data.Repositories.Contracts
 {
     public interface IBookRepository
     {
+        Task<Book[]> GetAllBooksByGenreAsync(string genre = "fiction");
     }
 }
