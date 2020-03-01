496b01d321b65d52e2acc0c018cf0c48392719e4
TonyStamatova <a.stamatova@abv.bg>
2020-03-01 20:12:50 +0200

    Added a view model for book

---
+++ LibraryApi/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryApi.Models
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Language { get; set; }
+
+        public ICollection<string> KeyWords { get; set; }
+
+        public string Description { get; set; }
+    }
+}
