d26c039b4905ccd993cd29d032dac4ca4e8b09e4
TonyStamatova <a.stamatova@abv.bg>
2020-03-03 21:57:54 +0200

    Added home page message

--- LibraryApi/App_Start/RouteConfig.cs
+++ LibraryApi/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,9 +14,9 @@ namespace LibraryApi
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapHttpRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                routeTemplate: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
---
+++ LibraryApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace LibraryApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Ok("The LibraryAPI is ready to take your requests...");
+        }
+    }
+}
