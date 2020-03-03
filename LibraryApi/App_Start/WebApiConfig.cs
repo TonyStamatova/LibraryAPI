2b0ecf55bbc01fd9e056deab0a00f75d501dff48
TonyStamatova <a.stamatova@abv.bg>
2020-03-03 16:58:27 +0200

    Changed serialization casing

--- LibraryApi/App_Start/WebApiConfig.cs
+++ LibraryApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -11,10 +12,17 @@ namespace LibraryApi
         {
             // Web API configuration and services
 
+            config.Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .ContractResolver = 
+                    new CamelCasePropertyNamesContractResolver();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
+            config.Routes
+                .MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
